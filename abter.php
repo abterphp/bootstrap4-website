6c2b94f8eb7b8ffc34573ad1a6efb151500f052b
Peter Aba <peter@peteraba.com>
2019-07-18 18:18:17 +0200

    Add frontend Form decoration

--- abter.php
+++ abter.php
@@ -2,6 +2,7 @@
 
 use AbterPhp\Bootstrap4Website\Bootstrappers;
 use AbterPhp\Bootstrap4Website\Events;
+use AbterPhp\Framework\Constant\Event;
 use AbterPhp\Framework\Constant\Module;
 use AbterPhp\Framework\Constant\Priorities;
 use AbterPhp\Website\Constant\Event as WebsiteEvent;
@@ -17,6 +18,10 @@ return [
         WebsiteEvent::WEBSITE_READY => [
             /** @see \AbterPhp\Bootstrap4Website\Events\Listeners\WebsiteDecorator::handle */
             Priorities::NORMAL => [sprintf('%s@handle', Events\Listeners\WebsiteDecorator::class)],
+        ],
+        Event::FORM_READY           => [
+            /** @see \AbterPhp\Bootstrap4Website\Events\Listeners\FormDecorator::handle */
+            Priorities::EXTREME_LOW => [sprintf('%s@handle', Events\Listeners\FormDecorator::class)],
         ],
     ],
     Module::RESOURCE_PATH      => realpath(__DIR__ . '/resources'),
---
+++ src/Events/Listeners/FormDecorator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbterPhp\Bootstrap4Website\Events\Listeners;
+
+use AbterPhp\Framework\Events\FormReady;
+use AbterPhp\Bootstrap4Website\Decorator\Form;
+
+class FormDecorator
+{
+    /** @var Form */
+    protected $formDecorator;
+
+    /**
+     * FormDecorator constructor.
+     *
+     * @param Form    $formDecorator
+     */
+    public function __construct(Form $formDecorator)
+    {
+        $this->formDecorator    = $formDecorator;
+    }
+
+    /**
+     * @param FormReady $event
+     */
+    public function handle(FormReady $event)
+    {
+        $form = $event->getForm();
+
+        $nodes = array_merge([$form], $form->getExtendedDescendantNodes());
+
+        $this->formDecorator->init()->decorate($nodes);
+    }
+}
